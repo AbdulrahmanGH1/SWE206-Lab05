0f4bc3a27e8bb7dcac02867074c0426d8614aabd
abulr <s202042560@kfupm.edu.sa>
2022-10-10 16:43:26 +0300

    First Commit

---
+++ src/com/company/Sorter.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Sorter {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        System.out.println("this line is for aziz (: ");
+    }
+}
